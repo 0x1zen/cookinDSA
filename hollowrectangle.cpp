3fefa900afebc16254a2c477bd93f7faedcce276
0x1zen <rajdubalwork@gmail.com>
2023-06-01 19:42:34 +0530

    Hollow rectangle pattern

---
+++ hollowrectangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int row=0;row<3;row++){
+        if(row==0 || row==2){
+       for(int col=0;col<5;col++){
+        cout<<"* ";
+       }
+    }
+    else{
+        cout<<"* ";
+        for(int col=0;col<3;col++){
+            cout<<"  ";
+        }
+        cout<<" *";
+    }
+    cout<<endl;
+}
+}
