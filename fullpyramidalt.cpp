75c4479e8d4a65afb0d94de4874ab8395735301b
0x1zen <rajdubalwork@gmail.com>
2023-06-12 16:31:57 +0530

    pattern:full pyramid alternative

---
+++ fullpyramidalt.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+for(int row=0;row<n;row++){
+    // Spaces
+    for(int col=0;col<n-row-1;col++){
+        cout<<" ";
+    }
+    // stars
+    for(int col=0;col<row+1;col++){
+        cout<<"* ";
+
+    }
+    cout<<endl;
+}
+}
