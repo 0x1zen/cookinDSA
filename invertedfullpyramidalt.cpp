ce59d717b626bf19b80493769cbf9f05ddc4bc28
0x1zen <rajdubalwork@gmail.com>
2023-06-13 20:15:09 +0530

    pattern:invertedfullpyramidalt

---
+++ invertedfullpyramidalt.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+for(int row=0;row<n;row++){
+    // spaces
+for(int col=0;col<row;col++){
+    cout<<" ";
+}
+for(int col=0;col<n-row;col++){
+cout<<"* ";
+}
+cout<<endl;
+}
+}
