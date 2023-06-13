e359755eef6cca68e62beb2b033eea7054e6ab42
0x1zen <rajdubalwork@gmail.com>
2023-06-13 20:14:07 +0530

    pattern:Hollow diamond

---
+++ hollowdiamond.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int row=0;row<n;row++){
+        for(int col=0;col<n-row-1;col++){
+            cout<<" ";
+        }
+        // stars
+        for(int col=0;col<2*row+1;col++){
+     if(col==0 || col==2*row){
+        cout<<"*";
+     }
+     else{
+        cout<<" ";
+     }
+        }
+        cout<<endl;
+
+    }
+    for(int row=0;row<n;row++){
+        // spaces
+    for(int col=0;col<row;col++){
+        cout<<" ";
+    }
+    // stars
+    for(int col=0;col<2*n-2*row-1;col++){
+         if(col==0 || col==2*n-2*row-2){
+        cout<<"*";
+     }
+     else{
+        cout<<" ";
+     }
+     
+    }
+cout<<endl;
+    }
+}
