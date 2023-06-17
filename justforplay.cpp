df31f7147041c8663e556c2686cd7c991527703f
0x1zen <rajdubalwork@gmail.com>
2023-06-17 11:22:57 +0530

    Some practice qs

--- justforplay.cpp
+++ justforplay.cpp
@@ -1,14 +1,66 @@
 #include<iostream>
 using namespace std;
+//  int n;
+//     cin>>n;
+// for(int row=0;row<n;row++){
+//     for(int col=0;col<n-row-1;col++){
+//         cout<<" ";
+//     }
+//     // Counting
+//     for(int col=0;col<row+1;col++){
+//         cout<<row+col+1;
+//     }
+//     // reverse couting
+//     int rev=2*row;
+//     for(int col=0;col<row;col++){
+// cout<<rev;
+// rev--;
+//     }
+//     cout<<endl;
+// }
+// int max(int a,int b){
+//     if(a>b){
+//         return a;
+//     }
+//     else{
+//         return b;
+//     }
+// }
 int main(){
-    int n;
-    cin>>n;
-    for(int r=0;r<n;r++){
-        for(int c=0;c<n;c++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
+    // int n;
+    // cin>>n;
+
+
+// int a=5;
+// int b=6;
+// cout<<max(a,b);
+
+// for(int row=0;row<n;row++){
+//     for(int col=0;col<n-row-1;col++){
+//         cout<<" ";
+//     }
+//     // Counting
+//     for(int col=0;col<row+1;col++){
+//         cout<<row+col+1;
+//     }
+//     // reverse couting
+//     int rev=2*row;
+//     for(int col=0;col<row;col++){
+// cout<<rev;
+// rev--;
+//     }
+//     cout<<endl;
+// }
+return -100;
+
+
+
+    // for(int r=0;r<n;r++){
+    //     for(int c=0;c<n;c++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
 // for(int row=0;row<n;row++){
 //     // Spaces
 //     for(int col=0;col<n-row-1;col++){
@@ -77,6 +129,7 @@ int main(){
 //      cout<<endl;
 // }
 }
+
 // for(int row=0;row<n;row++){
 //     // Spaces
 // for (int col=0;col<n-row-1;col++){
