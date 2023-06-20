aba861e49727836a7bf7209270cbe78136b2ef6c
0x1zen <rajdubalwork@gmail.com>
2023-06-20 19:38:48 +0530

    Linear search

---
+++ count0s1s.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={0,1,0,1,0,1,0,0,0,1,1,1,0,0,1};
+    int size=15;
+    int numZero=0;
+    int numOne=0;
+    for(int i=0;i<size;i++){
+        if(arr[i]==0){
+            numZero++;
+        }
+        if(arr[i]==1){
+            numOne++;
+        }
+    }
+    cout<<"The number of zeros are "<<numZero<<endl;
+    cout<<"The number of ones are "<<numOne<<endl;
+ return 0;
+}
