669b27457ca6fbed660abc54c0eb76e84c1b7b57
0x1zen <rajdubalwork@gmail.com>
2023-06-20 19:38:57 +0530

    Linear search

---
+++ linearsearch1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,3,4,5,6,7,8,9,100};
+    int maxx=INT_MIN;
+    int size=10;
+    for(int i=0;i<size;i++){
+        if(arr[i]>maxx){
+            maxx=arr[i];
+        }
+    }
+    cout<<"The max Number in this array is "<<maxx<<endl;
+ return 0;
+}
