2204a3016d64980bad8774e2147661bba712ec63
0x1zen <rajdubalwork@gmail.com>
2023-06-21 20:58:07 +0530

    Trying reverse array

---
+++ reversearray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,4,6,8,10};
+    int size=6;
+    int rev=size-1;
+ for(int i=0;i<size;i--){
+    cout<<rev;
+ }
+ return 0;
+}
