86f36ef002651a2bdc510cc30127891fc69f9020
0x1zen <rajdubalwork@gmail.com>
2023-06-23 12:53:09 +0530

    Intro to vectors

---
+++ vector.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<char>arr;
+    int ans=(sizeof(arr)/sizeof(int));
+    cout<<ans<<endl;
+    // cout<<sizeof(long long int)<<endl;
+ return 0;
+}
